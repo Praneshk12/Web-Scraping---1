0a045af9a0d6ef8abf078ea58483c5b9eb3240f8
Praneshk12 <77246096+Praneshk12@users.noreply.github.com>
2022-06-01 14:05:30 -0700

    Add files via upload

--- pro-c127/data_scraping.py
+++ pro-c127/data_scraping.py
@@ -1,38 +1,31 @@
-#writer - Pranesh Krishnaa
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
-
-page = requests.get(bright_stars_url)
-print(page)
-
-soup = bs(page.text,'html.parser')
-
-star_table = soup.find('table')
-
-temp_list= []
-table_rows = star_table.find_all('tr')
-for tr in table_rows:
-    td = tr.find_all('td')
+START_URL = (
+    "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+)
+wiki = requests.get(START_URL)
+soup = BeautifulSoup(wiki.text, "html.parser")
+temp_list = []
+for tr in soup.find("table").find_all("tr"):
+    td = tr.find_all("td")
     row = [i.text.rstrip() for i in td]
     temp_list.append(row)
 
-Star_names = []
-Distance =[]
-Mass = []
-Radius =[]
-Lum = []
+name = []
+distance = []
+mass = []
+radius = []
 
-for i in range(1,len(temp_list)):
-    Star_names.append(temp_list[i][1])
-    Distance.append(temp_list[i][3])
-    Mass.append(temp_list[i][5])
-    Radius.append(temp_list[i][6])
-    Lum.append(temp_list[i][7])
-    
-df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
-print(df2)
+for i in range(1, len(temp_list)):
+    name.append(temp_list[i][1])
+    distance.append(temp_list[i][3])
+    mass.append(temp_list[i][5])
+    radius.append(temp_list[i][6])
 
-df2.to_csv('bright_stars.csv')
+df = pd.DataFrame(
+    list(zip(name, distance, mass, radius)),
+    columns=["Star_name", "Distance", "Mass", "Radius"],
+)
+df.to_csv("data.csv")
